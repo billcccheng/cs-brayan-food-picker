c47baaacb98f35f620e79fcbb01c1c184b345e5d
Bill Cheng <chun-chan-cheng@viasat.com>
2017-04-12 13:40:59 -0500

    changed layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Food }        from '../food';
 
 @Component({
     selector: 'home',
-    templateUrl:'home.component.html'
+    templateUrl:'home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
